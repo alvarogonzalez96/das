1702fa020a786b0a9ee3bf0fceb451bb1e47c781
alvarogonzalez96 <43268965+alvarogonzalez96@users.noreply.github.com>
2020-12-23 22:11:04 +0100

    Updated Post, address and size added to db

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, address, size'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -24,19 +24,25 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        address = request.form['address']
+        size = request.form['size']
         error = None
 
         if not title:
             error = 'Title is required.'
+        elif not address: 
+        	error = 'Address is required'
+        elif not size:
+        	error = 'Size is required'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO post (title, body, author_id, address, size)'
+                ' VALUES (?, ?, ?, ?, ?)',
+                (title, body, g.user['id'], address, size)
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -45,7 +51,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, address, size'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -67,19 +73,25 @@ def update(id):
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        address = request.form['address']
+        size = request.form['size']
         error = None
 
         if not title:
             error = 'Title is required.'
+        elif not address:
+        	error = 'Address is required'
+        elif not size:
+        	error = 'Size ir required'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE post SET title = ?, body = ?, address = ?, size = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (title, body, id, address, size)
             )
             db.commit()
             return redirect(url_for('blog.index'))
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -14,5 +14,7 @@ CREATE TABLE post (
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
+  address TEXT NOT NULL,
+  size TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
